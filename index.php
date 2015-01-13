dc53aeecf26b719d02648c28dc675639ab713e55
pacmelanie <pacheco.melanie2a@gmail.com>
2015-01-13 16:38:30 +0100

    avance

--- index.php
+++ index.php
@@ -30,6 +30,20 @@ $page = (isset($_GET['page'])) ? htmlentities($_GET['page']) : NULL;
 
         <!-- Custom styles for this template -->
         <link href="style/global.css" rel="stylesheet">
+        <script type="text/javascript"
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfJL3M19Gdb4RTyDED_H3dfSmpCJhblNo" width="640" height="480">
+        </script>
+        <script type="text/javascript">
+            function initialize() {
+                var mapOptions = {
+                    center: {lat: -34.397, lng: 150.644},
+                    zoom: 8
+                };
+                var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+            }
+            google.maps.event.addDomListener(window, 'load', initialize);
+
+        </script>
     </head>
 
     <body>
