e90d5c96b7b189a14e29df6b3d232c139b46c144
Valentin PASSE <valentin-passe@hotmail.fr>
2015-01-14 12:08:46 +0100

    update

--- requetes/req_paint.php
+++ requetes/req_paint.php
@@ -9,14 +9,12 @@
 session_start();
 //inclusion du fichier connexion base de données
 include('../config/connexionBDD.php');
-echo "a";
 // récupérer les éléments du formulaire
 // et se protéger contre l'injection MySQL (plus de détails ici: http://us.php.net/mysql_real_escape_string)
 $idUser=$_SESSION['id'];
 $name=stripcslashes($_POST['name']);
 $drawingCommands=stripcslashes($_POST['drawingCommands']);
 $picture=stripcslashes($_POST['picture']);
-echo "b";
 
 try {
         // Tenter d'insérer le dessin dans la base
@@ -32,9 +30,7 @@ try {
             $err = $sql->errorInfo();
             print_r($err);
         }
-        echo "c";
         header("Location: ../index.php?page=pictionary.tpl");
-        echo "d";
 } 
 catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
