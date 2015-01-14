717f8e1e52cd4a5850f89fb9c7ff425ac023891b
Valentin PASSE <valentin-passe@hotmail.fr>
2015-01-14 10:49:03 +0100

    aa

--- js/pictionary.js
+++ js/pictionary.js
@@ -48,11 +48,13 @@
             // crér un nouvel objet qui représente une commande de type "start", avec la position, la couleur  
             var command = {};  
             command.command="start";  
-            command = {"command":"start", "x": (e.x - document.getElementById('Container').offsetLeft - document.getElementById('canvasPictionary').offsetLeft), "y":(e.y - document.getElementById('Container').offsetTop - document.getElementsByTagName('header')[0].offsetHeight), "size" : size, "color" : {"r" : hexToRgb(color).r, "g" : hexToRgb(color).g, "b" : hexToRgb(color).b, "a" : 0.9}};  
+            command = {"command":"start", "x": (e.x - document.getElementById('Container').offsetLeft - document.getElementById('canvasPictionary').offsetLeft), "y":(e.y - document.getElementById('Container').offsetTop), "size" : size, "color" : {"r" : hexToRgb(color).r, "g" : hexToRgb(color).g, "b" : hexToRgb(color).b, "a" : 0.9}};  
             // Ce genre d'objet Javascript simple est nommé JSON. Pour apprendre ce qu'est le JSON, c.f. http://blog.xebia.fr/2008/05/29/introduction-a-json/  
+
 
             // on l'ajoute à la liste des commandes  
             drawingCommands.push(command);
+
             console.log("Taille : "+command.size+" Couleur : " + "rgba("+command.color.r+","+command.color.g+","+command.color.b+","+command.color.a+")" + " x : "+ command.x + " y : "+ command.y);            
 
 
@@ -91,6 +93,7 @@
         document.getElementById('validate').onclick = function() {  
             // la prochaine ligne transforme la liste de commandes en une chaîne de caractères, et l'ajoute en valeur au champs "drawingCommands" pour l'envoyer au serveur.  
             document.getElementById('drawingCommands').value = JSON.stringify(drawingCommands);  
+            console.log(document.getElementById('drawingCommands').value);
 
             // ici, exportez le contenu du canvas dans un data url, et ajoutez le en valeur au champs "picture" pour l'envoyer au serveur.  
         };  
