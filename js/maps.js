e818b7aad4e6ade4303b3c5cba44040cd84721bf
Vincent FILLON <v.fillon.pro@gmail.com>
2015-01-14 09:51:04 +0100

    bug correction

--- js/maps.js
+++ js/maps.js
@@ -12,12 +12,4 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 }
-
-function loadScript() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-    'callback=initialize';
-    document.body.appendChild(script);
-}
-window.onload = loadScript;
+google.maps.event.addDomListener(window, 'load', initialize);
